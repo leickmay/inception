89877dc009d9d840f79ebd46f1dcbbd02cd6993d
Loïc Eickmayer <71378550+leickmay@users.noreply.github.com>
2021-10-19 21:33:04 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,7 +2,6 @@ sudo docker-compose -f srcs/docker-compose.yml down
 sudo docker rmi -f $(sudo docker images -qa)
 sudo docker rm -f $(sudo docker ps -qa)
 sudo docker volume rm $(sudo docker volume ls -q)
-#sudo docker system prune -a --volumes
 sudo docker system prune -a --force
 sudo rm -rf ../data/mariadb
 sudo rm -rf ../data/wordpress
